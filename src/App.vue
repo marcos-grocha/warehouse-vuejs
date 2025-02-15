3e0a9a2a1245d3ce4b07dd0b0dcf058c5a03ef81
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-02-15 01:12:58 -0300

    Criando o component Menu

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,27 @@
 <template>
   <div id="app">
-    <nav> <!-- Aqui cria links / rotas -->
-      <!-- rota: Home -->
-      <router-link to="/">Home</router-link> |
-      <!-- rota: About -->
-      <router-link to="/about">Sobre</router-link> |
-      <router-link to="/warehouses">Galpões Cadastrados</router-link> |
-      <router-link to="/warehouses/new">Cadastrar Galpão</router-link>
-    </nav>
-
+    <Menu/>
     <router-view/> <!-- Aqui carrega o conteúdo de forma dinâmica -->
   </div>
 </template>
 
+<script>
+  import Menu from './components/Menu.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Menu
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav> <!-- Aqui cria links / rotas -->
+      <!-- rota: Home -->
+      <router-link to="/">Home</router-link> |
+      <!-- rota: About -->
+      <router-link to="/about">Sobre</router-link> |
+      <router-link to="/warehouses">Galpões Cadastrados</router-link> |
+      <router-link to="/warehouses/new">Cadastrar Galpão</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Menu'
+    }
+</script>
+
+<style>
+    nav {
+    padding: 30px;
+    }
+
+    nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    }
+
+    nav a.router-link-exact-active {
+    color: #42b983;
+    }
+</style>
