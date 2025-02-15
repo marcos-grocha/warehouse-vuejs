e13ad6d7da6a7e5a9240b4ad429da791da10e771
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-02-15 02:36:53 -0300

    Consumindo dados da API em Rails WareHouse - Ao clicar no botão carregar dados, recebe no console

--- src/views/WareHouseList.vue
+++ src/views/WareHouseList.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <h1>Galpões Cadastrados</h1>
+        <button v-on:click="getWareHouse">Carregar dados</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'WareHouseList'
+        name: 'WareHouseList',
+        methods: {
+            async getWareHouse(){
+                // Realizando a requisição
+                const response = await this.$http.get('http://localhost:3000/api/v1/warehouses');
+
+                // Expõe a resposta da requisição
+                const result = await response.json();
+
+                // Exibindo os dados no console do navegador
+                console.log(result);
+            }
+        }
     }
 </script>
 
