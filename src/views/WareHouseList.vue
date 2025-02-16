634323cfed5c26d447b00368adabb95195c183cd
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-02-16 01:53:49 -0300

    Exibindo galpões cadastrados

--- src/views/WareHouseList.vue
+++ src/views/WareHouseList.vue
@@ -1,15 +1,38 @@
 <template>
     <div>
         <h1>Galpões Cadastrados</h1>
+
+        <table border="1px">
+            <thead>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Sigla</th>
+            </thead>
+            <tbody>
+                <tr v-for="w in warehouses" :key="w.code">
+                    <td>{{ w.id }}</td>
+                    <td>{{ w.name }}</td>
+                    <td>{{ w.code }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
     export default {
         name: 'WareHouseList',
+
+        data(){
+            return {
+                warehouses:[]
+            }
+        },
+
         async mounted(){
             this.getWareHouses();
         },
+
         methods: {
             async getWareHouses(){
                 // Realizando a requisição
@@ -20,6 +43,10 @@
 
                 // Exibindo os dados no console do navegador
                 console.log(result);
+
+                // Adicionando os dados de result no array warehouses (data)
+                this.warehouses = result;
+                return this.warehouses;
             }
         }
     }
